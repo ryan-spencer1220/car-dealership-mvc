a9c9bf8a0a20f20e079efeee6ff6badd760c6b1a
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-01 11:33:44 -0800

    Complete project files
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>
    Andy Plymate <Plymatea@users.noreply.github.com>

--- CarDealership/Models/CarDealership.cs
+++ CarDealership/Models/CarDealership.cs
@@ -21,12 +21,13 @@ namespace CarDealership.Models {
       Miles = miles;
       _cars.Add(this);
     }
+
+    private static  List<Car> _cars = new List<Car>() { };
       // Car volkswagen = new Car(1974 ,"Volkswagen", "Thing", 1100, 368792);
       // Car yugo = new Car(1980, "Yugo", "Koral", 700, 56000);
       // Car ford = new Car(1988, "Ford", "Country Squire", 1400, 239001);
       // Car amc = new Car(1976, "AMC", "Pacer", 400, 198000);
 
-    private static  List<Car> _cars = new List<Car>() {};
     public static List<Car> GetAll() 
     {
       return _cars;
